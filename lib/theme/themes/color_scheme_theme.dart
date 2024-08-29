71f9044e8837769a86e62817d7aee445bd2f055c
Matthew <mattscavone11@gmail.com>
2024-08-29 06:26:54 -0400

    Improvements to inputDecoration theme.

--- lib/theme/themes/color_scheme_theme.dart
+++ lib/theme/themes/color_scheme_theme.dart
@@ -19,7 +19,7 @@ ColorScheme darkScheme = const ColorScheme(
   surface: Color.fromARGB(255, 34, 34, 34), 
   onSurface: Color(0xFF66b2b2),
   primary: Color.fromARGB(255, 51, 51, 51), 
-  onPrimary: Color(0xFF100000), 
+  onPrimary: Colors.white, 
   secondary: Color(0xFF5e6572), 
   onSecondary: Color(0xFFf68e5f),
   error: Color(0xFFae3e0a), 
--- lib/theme/themes/input_decoration_theme.dart
+++ lib/theme/themes/input_decoration_theme.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:pet_profile_app/theme/themes/color_scheme_theme.dart';
 
 class CInputDecorationTheme {
   CInputDecorationTheme._();
 
-  static InputDecorationTheme lightInputDecorationTheme = const InputDecorationTheme(
-
+  static InputDecorationTheme lightInputDecorationTheme = InputDecorationTheme(
+    hintStyle: TextStyle(color: lightScheme.onPrimary),
   );
-  static InputDecorationTheme darkInputDecorationTheme = const InputDecorationTheme(
-    
+  static InputDecorationTheme darkInputDecorationTheme = InputDecorationTheme(
+    hintStyle: TextStyle(color: darkScheme.onPrimary),
   );
 }

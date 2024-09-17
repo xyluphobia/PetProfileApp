2945b1cf6a06f556020a3de2e2163f9b31b8ff3d
Matthew <matt@Matthews-Mac-mini.local>
2024-09-17 14:44:49 -0400

    Basic layout for profile page using updated class.

--- lib/account_details.dart
+++ lib/account_details.dart
@@ -4,29 +4,29 @@ class Account {
     this.lastLatLng,
     this.image,
     this.name,
-    this.age,
-    this.birthday,
+    this.homeAddress,
+    this.vetAddress, 
   });
 
   String? lastLatLng;
   String? image;
   String? name;
-  String? age;
-  String? birthday;
+  String? homeAddress;
+  String? vetAddress;
 
   factory Account.fromJson(Map<String, dynamic> json) => Account(
     lastLatLng: json["lastLatLng"],
     image: json["image"],
     name: json["name"],
-    age: json["age"],
-    birthday: json["birthday"],
+    homeAddress: json["homeAddress"], 
+    vetAddress: json["vetAddress"], 
   );
 
   Map<String, dynamic> toJson() => {
     "lastLatLng": lastLatLng,
     "image": image,
     "name": name,
-    "age" : age,
-    "birthday" : birthday,
+    "homeAddress": homeAddress,
+    "vetAddress": vetAddress,
   };
 }
--- lib/account_view.dart
+++ lib/account_view.dart
@@ -17,54 +17,114 @@ class _AccountViewState extends State<AccountView> {
   Widget build(BuildContext context) {
     account = context.watch<FileController>().accountDetails != null ? context.watch<FileController>().accountDetails! : Account();
 
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 48,
-                  backgroundColor: Colors.amber,
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 4,
+          bottom: 8,
+          right: 16,
+          left: 16,
+         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 64,
+                      backgroundColor: Colors.amber,
+                    ),
+                    Text(
+                      account.name != null ? account.name! : "First Last",
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                  ],
                 ),
-                Text(account.name != null ? account.name! : "First Last"),
-              ],
-            )
-          ),
-          const SizedBox(
-            child:Column(
-              children: [
-                Text("Contact"),
-                Text("919 648 95003"),
-                Text("mattscav@gmail.com"),
-              ],
+              )
             ),
-          ),
-          const SizedBox(
-            child:Column(
-              children: [
-                Text("Locations"),
-                Text("406 Summerlin Dr Goldsboro NC 27530 United States"),
-                Text("812 Moonerjin St Silverboro SC 30527 Australia"),
-              ],
+            Card(
+              child:Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Contact",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Text(
+                      "919 648 9503",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text(
+                      "mattscav@gmail.com",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-          FloatingActionButton(
-            heroTag: "temp1",
-            onPressed: () async {
-              account.name = "trial";
+            Card(
+              child:Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Locations",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Text(
+                      account.homeAddress != null ? account.homeAddress! : "406 Summerlin Dr Goldsboro NC 27530 United States",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text(
+                      account.vetAddress != null ? account.vetAddress! : "812 Moonerjin St Silverboro SC 30527 Australia",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Preferred Vet",
+                      style: Theme.of(context).textTheme.headlineMedium?.copyWith(color: Colors.red),
+                    ),
+                    Text(
+                      "name, phone #, address",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ],
+                ),
+              ),
+            ),
 
-              await context.read<FileController>().writeAccountDetails(account);
-            }
-          ),
-          FloatingActionButton(
-            heroTag: "temp2",
-            onPressed: () {
-              context.read<FileController>().clearAccountDetailsJson();
-            }
-          ),
-        ],
+            FloatingActionButton(
+              heroTag: "temp1",
+              onPressed: () async {
+                account.name = "trial";
+                account.homeAddress = "406 Summerlin Dr Goldsboro";
+                account.vetAddress = "812 Moonerjin St Silverboro";
+
+                await context.read<FileController>().writeAccountDetails(account);
+              },
+              child: const Text("Test Button 1"),
+            ),
+            FloatingActionButton(
+              heroTag: "temp2",
+              onPressed: () {
+                context.read<FileController>().clearAccountDetailsJson();
+              },
+              child: const Text("Reset Button"),
+            ),
+          ],
+        ),
       ),
     );
   }

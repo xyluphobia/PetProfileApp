6ceba748a480701798681bfaa8b89f1a25686b5e
Matthew <mattscavone11@gmail.com>
2024-10-23 16:30:22 -0400

    Design improvements to the account menu.

--- lib/account_view.dart
+++ lib/account_view.dart
@@ -15,9 +15,21 @@ class _AccountViewState extends State<AccountView> {
   late Account account;
   bool unsavedChanges = false;
 
+  final TextEditingController accNameController = TextEditingController();
+  final TextEditingController accPrefVetAddress = TextEditingController();
+  final TextEditingController accPrefVetNum = TextEditingController();
+  final TextEditingController accEmeVetAddress = TextEditingController();
+  final TextEditingController accEmeVetNum = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     account = context.watch<FileController>().accountDetails != null ? context.watch<FileController>().accountDetails! : Account();
+
+    if (account.name != null) accNameController.text = account.name!;
+    if (account.preferredVet.address != null) accPrefVetAddress.text = account.preferredVet.address!;
+    if (account.preferredVet.phoneNumber != null) accPrefVetNum.text = account.preferredVet.phoneNumber!;
+    if (account.emergencyVet.address != null) accEmeVetAddress.text = account.emergencyVet.address!;
+    if (account.emergencyVet.phoneNumber != null) accEmeVetNum.text = account.emergencyVet.phoneNumber!;
 
     return Center(
       child: Container(
@@ -48,26 +60,30 @@ class _AccountViewState extends State<AccountView> {
                         style: Theme.of(context).textTheme.headlineSmall,
                       ),
                     ),
-                    SizedBox( // Account name
-                      height: 24,
-                      child: TextField(
-                        keyboardType: TextInputType.name,
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                        autocorrect: false,
-                        maxLines: 1,
-                        enabled: true,
-                        
-                        onChanged: (text) {
-                          unsavedChanges = true;
-                          account.name = text;
-                        },
-                        decoration: InputDecoration(
-                          hintText: account.name ?? "Enter your name...",
-                          hintStyle: Theme.of(context).textTheme.bodyLarge,
-                          isDense: true,
-                          disabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: SizedBox( // Account name
+                        height: 24,
+                        child: TextField(
+                          keyboardType: TextInputType.name,
+                          textAlign: TextAlign.start,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          autocorrect: false,
+                          maxLines: 1,
+                          controller: accNameController,
+                          enabled: true,
+                          
+                          onChanged: (text) {
+                            unsavedChanges = true;
+                            account.name = text;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Enter your name...",
+                            hintStyle: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                            isDense: true,
+                            disabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                            ),
                           ),
                         ),
                       ),
@@ -80,57 +96,65 @@ class _AccountViewState extends State<AccountView> {
                         style: Theme.of(context).textTheme.headlineSmall,
                       ),
                     ),
-                    SizedBox( // preferred vet address
-                      height: 24,
-                      child: TextField(
-                        keyboardType: TextInputType.streetAddress,
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                        autocorrect: false,
-                        maxLines: 1,
-                        enabled: true,
-                        
-                        onChanged: (text) {
-                          // validate address then if it passes:
-                          // format address
-                          unsavedChanges = true;
-                          account.preferredVet.address = text;
-                        },
-                        decoration: InputDecoration(
-                          hintText: account.preferredVet.address ?? "Enter address...",
-                          hintStyle: Theme.of(context).textTheme.bodyLarge,
-                          isDense: true,
-                          disabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: SizedBox( // preferred vet address
+                        height: 24,
+                        child: TextField(
+                          keyboardType: TextInputType.streetAddress,
+                          textAlign: TextAlign.start,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          autocorrect: false,
+                          maxLines: 1,
+                          controller: accPrefVetAddress,
+                          enabled: true,
+                          
+                          onChanged: (text) {
+                            // validate address then if it passes:
+                            // format address
+                            unsavedChanges = true;
+                            account.preferredVet.address = text;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Enter address...",
+                            hintStyle: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                            isDense: true,
+                            disabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                    SizedBox( // Preferred vet phone number
-                      height: 24,
-                      child: TextField(
-                        keyboardType: TextInputType.phone,
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                        autocorrect: false,
-                        maxLines: 1,
-                        enabled: true,
-                        inputFormatters: [
-                          MaskedInputFormatter('(###) ###-####')
-                        ],
-                        
-                        onChanged: (text) {
-                          // validate phone number then if it passes:
-                    
-                          unsavedChanges = true;
-                          account.preferredVet.phoneNumber = text;
-                        },
-                        decoration: InputDecoration(
-                          hintText: account.preferredVet.phoneNumber ?? "Enter phone number...",
-                          hintStyle: Theme.of(context).textTheme.bodyLarge,
-                          isDense: true,
-                          disabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: SizedBox( // Preferred vet phone number
+                        height: 24,
+                        child: TextField(
+                          keyboardType: TextInputType.phone,
+                          textAlign: TextAlign.start,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          autocorrect: false,
+                          maxLines: 1,
+                          controller: accPrefVetNum,
+                          enabled: true,
+                          inputFormatters: [
+                            MaskedInputFormatter('(###) ###-####')
+                          ],
+                          
+                          onChanged: (text) {
+                            // validate phone number then if it passes:
+                      
+                            unsavedChanges = true;
+                            account.preferredVet.phoneNumber = text;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Enter phone number...",
+                            hintStyle: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                            isDense: true,
+                            disabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                            ),
                           ),
                         ),
                       ),
@@ -143,57 +167,65 @@ class _AccountViewState extends State<AccountView> {
                         style: Theme.of(context).textTheme.headlineSmall,
                       ),
                     ),
-                    SizedBox( // Emergency vet address
-                      height: 24,
-                      child: TextField(
-                        keyboardType: TextInputType.streetAddress,
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                        autocorrect: false,
-                        maxLines: 1,
-                        enabled: true,
-                        
-                        onChanged: (text) {
-                          // validate address then if it passes:
-                          // format address
-                          unsavedChanges = true;
-                          account.emergencyVet.address = text;
-                        },
-                        decoration: InputDecoration(
-                          hintText: account.emergencyVet.address ?? "Enter address...",
-                          hintStyle: Theme.of(context).textTheme.bodyLarge,
-                          isDense: true,
-                          disabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: SizedBox( // Emergency vet address
+                        height: 24,
+                        child: TextField(
+                          keyboardType: TextInputType.streetAddress,
+                          textAlign: TextAlign.start,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          autocorrect: false,
+                          maxLines: 1,
+                          controller: accEmeVetAddress,
+                          enabled: true,
+                          
+                          onChanged: (text) {
+                            // validate address then if it passes:
+                            // format address
+                            unsavedChanges = true;
+                            account.emergencyVet.address = text;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Enter address...",
+                            hintStyle: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                            isDense: true,
+                            disabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                    SizedBox( // Emergency vet phone number
-                      height: 24,
-                      child: TextField(
-                        keyboardType: TextInputType.phone,
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                        autocorrect: false,
-                        maxLines: 1,
-                        enabled: true,
-                        inputFormatters: [
-                          MaskedInputFormatter('(###) ###-####')
-                        ],
-                        
-                        onChanged: (text) {
-                          // validate phone number then if it passes:
-                    
-                          unsavedChanges = true;
-                          account.emergencyVet.phoneNumber = text;
-                        },
-                        decoration: InputDecoration(
-                          hintText: account.emergencyVet.phoneNumber ?? "Enter phone number...",
-                          hintStyle: Theme.of(context).textTheme.bodyLarge,
-                          isDense: true,
-                          disabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: SizedBox( // Emergency vet phone number
+                        height: 24,
+                        child: TextField(
+                          keyboardType: TextInputType.phone,
+                          textAlign: TextAlign.start,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          autocorrect: false,
+                          maxLines: 1,
+                          controller: accEmeVetNum,
+                          enabled: true,
+                          inputFormatters: [
+                            MaskedInputFormatter('(###) ###-####')
+                          ],
+                          
+                          onChanged: (text) {
+                            // validate phone number then if it passes:
+                      
+                            unsavedChanges = true;
+                            account.emergencyVet.phoneNumber = text;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Enter phone number...",
+                            hintStyle: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).colorScheme.secondary),
+                            isDense: true,
+                            disabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Theme.of(context).colorScheme.onSurface),
+                            ),
                           ),
                         ),
                       ),

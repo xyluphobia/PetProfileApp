9215b0abc06d20aeeac844e242246802f28a3d14
Matthew <mattscavone11@gmail.com>
2024-10-25 20:09:46 -0400

    Added image of developer to about/contact page.

--- lib/contact_us_view.dart
+++ lib/contact_us_view.dart
@@ -22,7 +22,38 @@ class _ContactUsViewState extends State<ContactUsView> {
           },
         ),
       ),
-      body: const Placeholder(),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 16,
+          bottom: 8,
+          right: 16,
+          left: 16,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundColor: Theme.of(context).colorScheme.onSurface,
+                    backgroundImage: const Image(image: AssetImage('assets/images/matthewAboutPicture.jpg')).image,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 140,
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ),
+            const Card(),
+            const Card(),
+          ],
+        ),
+      ),
     );
   }
 }
